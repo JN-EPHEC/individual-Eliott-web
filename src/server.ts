7d65ee0ed215dff018c333c2db681483fdee324c
Eliott-web <he202397@students.ephec.be>
2026-02-16 13:35:35 +0100

    serveur API

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req : Request, res : Response) => {
+  res.send('Bienvenue sur mon serveur API');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
 function greet(name: string): string {
     return `Hello ${name}, welcome to TypeScript!`;
 }
