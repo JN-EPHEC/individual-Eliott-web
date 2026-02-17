f9ab9aceece93ee095d2842bb2415900cf72f64c
Eliott-web <he202397@students.ephec.be>
2026-02-17 15:06:53 +0100

    userRouter

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import userRoutes from './routes/userRoutes';
+
 
 const app = express();
 const port = 3000;
+const etudiants = [
+{ id: 1, nom: "Dupont", prenom: "Jean" },
+{ id: 2, nom: "Martin", prenom: "Sophie" },
+{ id: 3, nom: "Doe", prenom: "John" },
+];
+
+app.get('/api/hello/:name', (req : Request, res : Response) => {
+  let retours = {message : `Bonjour ${req.params.name}`, timeStamp : new Date};
+  res.json(retours);
+});
 
 app.get('/', (req : Request, res : Response) => {
   res.send('Bienvenue sur mon serveur API');
@@ -10,6 +22,12 @@ app.get('/', (req : Request, res : Response) => {
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://localhost:${port}`);
 });
+
+app.get('/api/data', (req : Request, res : Response) => {
+  res.json(etudiants);
+});
+
+app.use('/api', userRoutes);
 
 function greet(name: string): string {
     return `Hello ${name}, welcome to TypeScript!`;
